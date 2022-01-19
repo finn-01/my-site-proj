c6bfd034320ab2e825acbaa0ad650849282ed3e2
finn-01 <nguyensam576@gmail.com>
2022-01-20 00:33:01 +0700

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-	return <div>Footer Components</div>;
+	return (
+		<div className="text-center flicker-5">
+			Copyright © 2022 All rights reserved | This website is made with by Finn
+		</div>
+	);
 };
 
 export default Footer;
